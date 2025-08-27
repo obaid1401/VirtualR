0efe0fff8e9073a54559fd741d20b2a520c0ed87
obaid1401 <bediobaid@gmail.com>
2025-08-28 00:07:06 +0500

    Features Workflow and Price Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import Navbar from  './components/navbar'
 import HeroSection from './components/heroSection'
+import Feather from './components/feature';
+import Workflow from './components/workflow';
+import Price from './components/Price';
+
 import './App.css'; 
 
 function App() {
@@ -12,8 +16,14 @@ function App() {
     <Navbar/>
     <div className='max-w-7xl mx-auto pt-20 px-6'>
       <HeroSection/>
+      <Feather/>
+      <Workflow/>
+      <Price/>
     </div>
-    
+
+    {/* <div className='max-w-7xl mx-auto pt-20 px-6'>
+    </div>
+     */}
     </>
   );
 }
